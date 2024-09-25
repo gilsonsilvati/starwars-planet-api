8fab79eae2856f01a91064347b722169ab9737f1
Gilson Silva <gilsonsilvati@gmail.com>
2024-09-24 21:12:19 -0300

    test: Removido contexto do spring e add ExtendWith MockitoExtension

--- src/test/java/br/com/starwars/domain/PlanetServiceTest.java
+++ src/test/java/br/com/starwars/domain/PlanetServiceTest.java
@@ -2,35 +2,32 @@ package br.com.starwars.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static br.com.starwars.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = PlanetService.class)
+@ExtendWith(MockitoExtension.class)
 class PlanetServiceTest {
 
-    @MockBean
+    @Mock
     PlanetRepository repository;
 
-    @Autowired
+    @InjectMocks
     PlanetService service;
 
     @Test
     @DisplayName("Create Planet with valid data return Planet")
     void createPlanet_WithValidData_ReturnPlanet() {
-        // AAA
-        // Arrange
         when(repository.save(PLANET)).thenReturn(PLANET);
 
-        // Act
         // sut: system under test (sistema em teste)
         var sut = service.create(PLANET);
 
-        // Assert
         assertThat(sut).isNotNull();
         assertThat(sut).isEqualTo(PLANET);
     }
