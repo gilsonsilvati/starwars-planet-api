d1c4eaef18306b71d736271051621d3c8a93cf9d
Gilson Silva <gilsonsilvati@gmail.com>
2024-10-01 14:49:38 -0300

    test: Configuração do banco de dados MySQL: profiles de tests

--- src/test/java/br/com/starwars/PlanetIT.java
+++ src/test/java/br/com/starwars/PlanetIT.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PlanetIT {
 
